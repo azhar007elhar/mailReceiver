7c379725d2dada8e6d22a525c51fa0f57901b329
azhar.elhar <azhar7elhar@gmail.com>
2023-03-09 01:05:49 +0100

    first commit

---
+++ app/Http/Controllers/MailReceiverController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Webklex\IMAP\Facades\Client;
+
+class MailReceiverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        try {
+            /* Alternative by using the Facade */
+            // $oClient = \Webklex\IMAP\Facades\Client::account('default');
+            // $oClient = \Webklex\IMAP\Facades\Client::account('default');
+
+            $options = [
+                'host'          => 'mail.crmdermafric.com',
+                'port'          => 993,
+                'encryption'    => 'ssl',
+                'validate_cert' => true,
+                'username'      => 'support@crmdermafric.com',
+                'password'      => 'phTMhc@I=^uH',
+                'protocol'      => 'imap'
+            ];
+            $oClient = Client::make($options);
+
+            //Connect to the IMAP Server
+            $oClient->connect();
+
+
+            //Get all Mailboxes
+            /** @var \Webklex\IMAP\Support\FolderCollection $aFolder */
+            $aFolder = $oClient->getFolders();
+
+            //Loop through every Mailbox
+            /** @var \Webklex\IMAP\Folder $oFolder */
+            $results = [];
+            foreach ($aFolder as $oFolder) {
+
+                //Get all Messages of the current Mailbox $oFolder
+                /** @var \Webklex\IMAP\Support\MessageCollection $aMessage */
+                // $aMessage = $oFolder->messages()->all()->get();
+
+                // get unread messages 
+                $aMessage = $oFolder->search()->unseen()->get();
+
+
+                // dd($aMessage);
+
+                /** @var \Webklex\IMAP\Message $oMessage */
+                foreach ($aMessage as $oMessage) {
+                    $res = (object) [
+                        'uid' => $oMessage->uid,
+                        'Subject' =>  mb_decode_mimeheader($oMessage->getSubject()),
+                        'Attachments Count' =>  $oMessage->getAttachments()->count(),
+                        'Attachments' =>  $oMessage->getAttachments(),
+                        'HTML Body' =>  $oMessage->getHTMLBody(true),
+                        'body' => $oMessage->getTextBody(),
+                        'From' => $oMessage->getFrom()[0]->mail,
+                    ];
+                    array_push($results, $res);
+                    // echo $oMessage->getSubject() . '<br />';
+                    // echo 'Attachments: ' . $oMessage->getAttachments()->count() . '<br />';
+                    // echo $oMessage->getHTMLBody(true);
+
+                    //Move the current Message to 'INBOX.read'
+                    // if ($oMessage->moveToFolder('INBOX.read') == true) {
+                    //     echo 'Message has ben moved';
+                    // } else {
+                    //     echo 'Message could not be moved';
+                    // }
+                }
+            }
+
+            // $emails = array_reverse($results , true );
+            // Log::info(json_encode($emails));
+            return $results;
+        } catch (\Throwable $th) {
+            //throw $th;
+            Log::error($th->getMessage());
+            return $th->getMessage();
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
